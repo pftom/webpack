bf3f9d3a94b69907f6c70d43efcdde15304c61fd
黄巍 <huan1043269994@gmail.com>
2017-11-22 15:42:05 +0800

    [ADD] completed webpack guide learning.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,22 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    polyfills: './src/polyfills.js',
-    index: './src/index.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-    new webpack.ProvidePlugin({
-      join: ['lodash', 'join'],
-    }),
-  ],
+  entry: './src/index.ts',
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
   module: {
     rules: [
       {
-        test: require.resolve('./src/index.js'),
-        use: 'imports-loader?this=>window'
-      },
-      {
-        test: require.resolve('./src/globals.js'),
-        use: 'exports-loader?file,parse=helpers.parse'
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
-};
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
